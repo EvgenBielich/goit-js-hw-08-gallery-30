40d900a99ea0b9c4fc3d436d738b17cc2bc7d83d
Evgen Bielich <belichevgenij@gmail.com>
2021-04-22 18:50:42 +0300

    модалка

--- gallery-task.js
+++ gallery-task.js
@@ -27,3 +27,58 @@ function createImgGallery(gallerys) {
    .join(' ');
 };
 createImgGallery(gallerys);
+
+const ImageEl = document.querySelector('.gallery-image');
+console.log(ImageEl.src);
+
+
+imgContainer.addEventListener('click', onContainerClick);
+const modalImgRef = document.querySelector('.lightbox__image');
+const modalContainer = document.querySelector('.lightbox');
+
+function onContainerClick(event) {
+   event.preventDefault();
+   const isGallaryEl = event.target.classList.contains('gallery-image');
+   if (!isGallaryEl) {
+       return;
+   }
+   const currentActiveImg = document.querySelector('.lightbox .is-open')
+       
+   if (currentActiveImg) {
+       currentActiveImg.classList.remove('is-open');
+   }
+
+   window.addEventListener('keydown', onEscPress);
+  modalContainer.classList.add('is-open');
+   modalImgRef.src = event.target.dataset.source;
+   modalImgRef.alt = event.target.alt;
+   
+};
+
+const closeButtonEl = document.querySelector('.lightbox__button');
+console.log(closeButtonEl);
+const closeModalEl = document.querySelector('.lightbox__content');
+console.log(closeModalEl);
+const closeOverlayEl = document.querySelector('.lightbox__overlay');
+console.log(closeOverlayEl);
+
+closeOverlayEl.addEventListener('click', onCloseButton);
+closeButtonEl.addEventListener('click', onCloseButton);
+
+
+function onCloseButton(e) {
+    modalContainer.classList.remove('is-open');
+    modalImgRef.src = '';
+    modalImgRef.alt = '';
+    window.removeEventListener('keydown', onEscPress);
+    if (modalContainer.classList.contains('is-open')) {
+    window.removeEventListener('keydown', onArrowEl);
+  };
+};
+
+function onEscPress(event) {
+    console.log(event.code);
+    if (event.code === 'Escape') {
+        onCloseButton();
+    }
+};
